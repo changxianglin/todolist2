3a8fa3f7aa24cd3460f589e386465eecac3f0be3
changxianglin <281532612@qq.com>
2023-07-07 02:52:04 +0800

    use bootstrap.css render list

--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>todolist</div>
+<ul class="list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    A list item
+    <span class="badge badge-primary badge-pill">14</span>
+  </li>
+</ul>
 </template>
 
 <script>
@@ -8,5 +13,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .list-group {
+    width: 500px;
+    margin: 0 auto;
+  }
 </style>
