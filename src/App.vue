406cda8cc9c69de44dd080fda3ac0ce25f7b5c1c
changxianglin <281532612@qq.com>
2023-07-07 02:37:39 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>root</h1>
+    <todoList />
+  </div>
+</template>
+
+<script>
+import todoList from './components/todoList.vue';
+export default {
+  name: 'App',
+  components: {
+    todoList,
+  }
+}
+</script>
---
+++ src/components/todoList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>todolist</div>
+</template>
+
+<script>
+export default {
+  name: 'todoList',
+}
+</script>
+
+<style>
+</style>
