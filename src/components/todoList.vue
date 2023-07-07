8bc1e4d83a3f8533d43646810e30dedb09e798ef
changxianglin <281532612@qq.com>
2023-07-08 01:01:13 +0800

    add checed and status

--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -1,8 +1,15 @@
 <template>
 <ul class="list-group">
   <li class="list-group-item d-flex justify-content-between align-items-center">
-    A list item
-    <span class="badge badge-primary badge-pill">14</span>
+    <!-- 复选框 -->
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+      <label class="form-check-label" for="defaultCheck1">
+        Default checkbox
+      </label>
+    </div>
+    <span class="badge badge-primary badge-pill">已完成</span>
+    <span class="badge badge-warning badge-pill">未完成</span>
   </li>
 </ul>
 </template>
